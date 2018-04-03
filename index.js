e9153f3f41a51f0c919025d70bae4eb1a1bb3d34
simon3000 <simon3000@163.com>
2018-04-03 15:57:09 +0800

    Event sruffs

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const level = require('level')
+const EventEmitter = require('events').EventEmitter
 
 class engine {
   constructor(name, savefile) {
     this.name = name
-    this.db = level(savefile)
+    this.event = new EventEmitter()
+    level(savefile, (err, db) => {
+      if (err) throw err
+      this.db = db
+      this.on('')
+    })
+  }
+  emit(channel, value) {
+    this.event.emit(channel, value)
+  }
+  on(channel, callback) {
+    this.event.on(channel, callback)
   }
 }
 
