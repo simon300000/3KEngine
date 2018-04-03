bbcbcf5bcf06392e9412eddbe2598194f6d1fec1
simon3000 <simon3000@163.com>
2018-04-03 16:23:57 +0800

    add more unit test

--- test/test.js
+++ test/test.js
@@ -1,19 +1,28 @@
 const chai = require('chai')
 const assert = chai.assert
 const Engine = require('../')
+const fs = require('fs-extra')
 
-describe('Basic', () => {
-  it('module should output a function', () => {
-    assert.equal(typeof Engine, 'function')
-  })
-  let story
-  it('Able to new Engine() with version 0', (done) => {
-    story = new Engine('myStory', './save')
-    story.on('version', (v) => {
-      done(assert.equal(v, 0))
+
+describe('Test', () => {
+  after(() => {
+    fs.emptyDir('./save').then(()=>{
+      fs.rmdir('./save')
     })
   })
-  it('Which should be object', () => {
-    assert.equal(typeof story, 'object')
+  describe('Basic', () => {
+    it('module should output a function', () => {
+      assert.equal(typeof Engine, 'function')
+    })
+    let story
+    it('Able to new Engine() with version 0', (done) => {
+      story = new Engine('myStory', './save')
+      story.on('version', (v) => {
+        done(assert.equal(v, 0))
+      })
+    })
+    it('Which should be object', () => {
+      assert.equal(typeof story, 'object')
+    })
   })
 })
