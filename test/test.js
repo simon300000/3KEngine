ad18f31bd667c30bc5ddb092d69700677c36a73c
simon3000 <simon3000@163.com>
2018-04-03 16:36:20 +0800

    not use => in mocha test→_→

--- test/test.js
+++ test/test.js
@@ -4,24 +4,24 @@ const Engine = require('../')
 const fs = require('fs-extra')
 
 
-describe('Test', () => {
+describe('Test', function() {
   after(() => {
-    fs.emptyDir('./save').then(()=>{
+    fs.emptyDir('./save').then(() => {
       fs.rmdir('./save')
     })
   })
-  describe('Basic', () => {
-    it('module should output a function', () => {
+  describe('Basic', function() {
+    it('module should output a function', function() {
       assert.equal(typeof Engine, 'function')
     })
     let story
-    it('Able to new Engine() with version 0', (done) => {
+    it('Able to new Engine() with version 0', function(done) {
       story = new Engine('myStory', './save')
       story.on('version', (v) => {
         done(assert.equal(v, 0))
       })
     })
-    it('Which should be object', () => {
+    it('Which should be object', function() {
       assert.equal(typeof story, 'object')
     })
   })
