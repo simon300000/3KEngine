99836cd4fdd7d8d4dc711701f50d9ac61364f8e3
simon3000 <simon3000@163.com>
2018-04-03 15:21:20 +0800

    add unit test content

--- test/test.js
+++ test/test.js
@@ -6,4 +6,7 @@ describe('Basic', function() {
   it('module should output a function', function() {
     assert.equal(typeof Engine, 'function')
   })
+  it('Able to new Engine()', function() {
+    let story = new Engine('myStory', './save')
+  })
 })
