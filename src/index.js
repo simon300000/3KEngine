3ac462f37f165b7f20b5d9e3328adae9a367987d
simon3000 <simon3000@163.com>
2018-04-05 16:08:07 +0800

    Even smaller

--- src/index.js
+++ src/index.js
@@ -17,14 +17,12 @@ class engine {
   on(channel, callback) {
     this.event.on(channel, callback)
   }
-  version(v) {
-    return (async () => {
-      if (v === undefined) {
-        return this.db.get('version')
-      } else {
-        return this.db.put('version', v)
-      }
-    })()
+  async version(v) {
+    if (v === undefined) {
+      return this.db.get('version')
+    } else {
+      return this.db.put('version', v)
+    }
   }
   putChapter(chapter, array) {
     return this.db.batch(
@@ -40,47 +38,35 @@ class engine {
         }
       })))
   }
-  getChapter(chapter, index) {
+  async getChapter(chapter, index) {
     index = index || 0
-    return (async () => {
-      return this.db.get(`chapter_${chapter}_${index}`)
-    })()
+    return this.db.get(`chapter_${chapter}_${index}`)
   }
-  getPlayer(index) {
-    return (async () => {
-      return this.db.get(`player_${index}`)
-    })()
+  async getPlayer(index) {
+    return this.db.get(`player_${index}`)
   }
-  getPlayers() {
-    return (async () => {
-      return this.db.get(`player`)
-    })()
+  async getPlayers() {
+    return this.db.get(`player`)
   }
-  setPlayer(index, value) {
-    return (async () => {
-      let playerList = await this.db.get('player')
-      playerList[index] = true
-      await this.db.put('player', playerList)
-      return this.db.put(`player_${index}`, value)
-    })()
+  async setPlayer(index, value) {
+    let playerList = await this.db.get('player')
+    playerList[index] = true
+    await this.db.put('player', playerList)
+    return this.db.put(`player_${index}`, value)
   }
-  delPlayer(index) {
-    return (async () => {
-      let playerList = await this.db.get('player')
-      playerList[index] = false
-      return this.db.put('player', playerList)
-    })()
+  async delPlayer(index) {
+    let playerList = await this.db.get('player')
+    playerList[index] = false
+    return this.db.put('player', playerList)
   }
-  newPlayer() {
-    return (async () => {
-      let playerList = await this.db.get('player')
-      for (let i = 0; i < playerList.length; i++) {
-        if (!playerList[i]) {
-          return i
-        }
+  async newPlayer() {
+    let playerList = await this.db.get('player')
+    for (let i = 0; i < playerList.length; i++) {
+      if (!playerList[i]) {
+        return i
       }
-      return playerList.length
-    })()
+    }
+    return playerList.length
   }
 }
 
