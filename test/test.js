d9a05ae137d44853a7467744f1452d223e28991c
simon3000 <simon3000@163.com>
2018-04-06 22:34:13 +0800

    remove not used functions in test file

--- test/test.js
+++ test/test.js
@@ -18,7 +18,7 @@ describe('Test', function() {
     })
     it('Able to new Engine() with version 0', async function() {
       story = new Engine('myStory', './save')
-      let v = await (new Promise((resolve, reject) => {
+      let v = await (new Promise((resolve) => {
         story.on('ready', resolve)
       }))
       assert.equal(v, 0)
@@ -34,7 +34,7 @@ describe('Test', function() {
     let story2
     it('Now I could create second one with same databse but different name and got version 0', async function() {
       story = new Engine('myStory2', './save')
-      let v = await (new Promise((resolve, reject) => {
+      let v = await (new Promise((resolve) => {
         story.on('ready', resolve)
       }))
       assert.equal(v, 0)
