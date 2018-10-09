68a043addfdbfa76a59461237bf0921c8bdc8329
simon3000 <simon3000@163.com>
2018-10-09 11:27:37 +0800

    use json encoding

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ let databaseOpen = []
 let database = {}
 
 const storeDatabase = (name, file) => {
-  let db = level(file)
+  let db = level(file, { valueEncoding: 'json' })
   databaseOpen.push(file)
   database[file] = db
   return openDatabase(name, db)
@@ -23,9 +23,9 @@ const initDatabase = (name, db) => {
       .catch(e => {
         if (e.notFound) {
           db.batch()
-            .put(`${name}_version`, JSON.stringify(0))
-            .put(`${name}_player`, JSON.stringify([]))
-            .put(`${name}_config`, JSON.stringify({}))
+            .put(`${name}_version`, 0)
+            .put(`${name}_player`, [])
+            .put(`${name}_config`, {})
             .write()
             .then(resolve)
             .catch(console.error)
@@ -38,11 +38,11 @@ const initDatabase = (name, db) => {
 
 class DatabaseInstance {
   constructor(name, db) {
-    this.get = key => helper.promise(db.get(`${name}_${key}`), JSON.parse)
-    this.put = (key, value) => helper.promise(db.put(`${name}_${key}`, JSON.stringify(value)))
+    this.get = key => helper.promise(db.get(`${name}_${key}`))
+    this.put = (key, value) => helper.promise(db.put(`${name}_${key}`, value))
     this.batch = array => {
       let batch = db.batch()
-      array.map(element => batch[element.type](`${name}_${element.key}`, JSON.stringify(element.value)))
+      array.map(element => batch[element.type](`${name}_${element.key}`, element.value))
       return helper.promise(batch.write())
     }
     this.db = db
