cb092f672174d3a0dea9b16da26e15448515ad7b
wjstjfdmlehdanfdjffnrth <djffnrth2000@naver.com>
2019-08-23 11:31:39 +0900

    reading files from background dir

---
+++ src/BackgroundArray.c
@@ -0,0 +1,14 @@
+/*This program reads the total background image on a certain dir and returns
+with certain array for engine to load. Still working on it. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+struct dirent {
+  /* data */
+};
+
+// Not completed
